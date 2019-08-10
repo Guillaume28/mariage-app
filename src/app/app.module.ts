2972c62e68327c3a0bb8000d00b8e5b9a9455e16
Guillaume28 <gbaldo@centreon.com>
2019-08-10 15:48:46 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
