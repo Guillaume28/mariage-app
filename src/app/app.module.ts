489fa8cbcc52521480e01ba7055f09cac354588e
Guillaume28 <gbaldo@centreon.com>
2019-08-14 13:17:11 +0200

    Work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ImageListComponent } from './image-list/image-list.component';
-import { HeaderComponent } from './header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ImageListComponent} from './image-list/image-list.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule, MatListModule, MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatToolbarModule
+} from '@angular/material';
+import {MatGridListResponsiveModule} from '../lib/mat-grid-list-responsive.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageListComponent,
-    HeaderComponent
+    ImageListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+    MatGridListResponsiveModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/image-list/image-list.component.ts
+++ src/app/image-list/image-list.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface Tile {
+  cols: number;
+  rows: number;
+  src: any;
+}
+
+export interface Collection {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-image-list',
@@ -7,7 +18,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageListComponent implements OnInit {
 
-  constructor() { }
+  tiles: Tile[] = [
+    {cols: 1, rows: 1, src: '../assets/pictures/groupe_mariage.jpg'},
+    {cols: 2, rows: 2, src: '../assets/pictures/signature_Guillaume.jpg'},
+    {cols: 3, rows: 3, src: '../assets/pictures/signature_Marianne.jpg'},
+    {cols: 1, rows: 1, src: '../assets/pictures/guillaume_Marianne_drapeau.jpg'},
+  ];
+
+  collections: Collection[] = [
+    {value: 'preparation', viewValue: 'Préparation mariage'},
+    {value: 'mariage', viewValue: 'Mariage'},
+    {value: 'marche', viewValue: 'Marche dans la ville'},
+    {value: 'repas', viewValue: 'Repas'},
+    {value: 'apres-mariage', viewValue: 'Après-mariage'}
+  ];
+
+  responsive = true;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
